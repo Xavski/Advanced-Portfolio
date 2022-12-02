ec6cce5dfe55a6a333b053f9bf68c4e2115501e0
Xavski <xavioursaveski1@gmail.com>
2022-12-02 20:43:49 +1100

    trying to fix phone lag

--- background.js
+++ background.js
@@ -90,6 +90,9 @@ class Particle{
 function init(){
     particlesArray = []
     let numberOfParticles = (canvas.height * canvas.width)/50000
+    if(canvas.height * canvas.width < 916*450){
+        numberOfParticles = 10
+    }
     for(let i = 0; i < numberOfParticles * 2; ++i){
         let size = (Math.random()*5) + 1
         let x  = (Math.random() * ((innerWidth - size * 2) - (size * 2)) + size * 2)
